ea6113904c371f4d40cbb5e54c77c23a530516cf
Aditya24-rathore <ra4402538@email.com>
2025-04-25 05:25:23 -0700

    uptodate

---
+++ Datatype/ordereddatatype/list.py
@@ -0,0 +1,13 @@
+# list is a collection of homogenous as well as hetrogenous elements.
+# list inbuit function
+l=[2,4,6,8,]
+print(max(l))
+print(min(l))
+print(len(l))
+print(sum(l))
+print(type(l))
+print(id(l))
+
+# List method
+# append - add element in last
+
