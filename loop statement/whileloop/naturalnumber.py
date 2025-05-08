a50a16b2cc1e966a76c32b6ec82e805cdfb56639
Aditya24-rathore <ra4402538@email.com>
2025-05-08 04:04:41 -0700

    update

---
+++ loop statement/whileloop/naturalnumber.py
@@ -0,0 +1,27 @@
+n=int(input("enter number"))
+i=1
+while i<=n:
+    print(i,end=', ',sep=',')
+    i=i+1
+
+n=int(input("enter number"))
+i=1
+while i<=n:
+    if i<=(n-1):
+        print(i,end=',')
+    else:
+        print(i)
+    i=i+1
+
+# sum of natural number
+n=int(input("enter number"))
+i=1
+sum=0
+while i<=n:
+    sum=sum+i
+    if i<=(n-1):
+        print(i,end=',')
+    else:
+        print(i,end='=')
+    i=i+1
+print(sum)
