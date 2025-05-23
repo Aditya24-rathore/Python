233d14949664d9658fc8b1bc43a4a5c72ec34f79
Adityaraj Rathore <ra4402538@gmail.com>
2025-05-23 04:33:29 -0700

    update

--- OOPS/class.py
+++ OOPS/class.py
@@ -55,5 +55,40 @@ obj2.add(1234)
 print(obj2.n,obj2.p,obj2.c,obj2.school)
         
 # class/static variable:- do not depend on object
-# local variable:-scope dependent
+class Student:
+    school="XYZ"
+    def __init__ (self,name):
+        self.n=name
+        Student.schoolcode=101
+    def addnew(self):
+        Student.schoolcity="Bhopal"
+        print(Student.schoolcity)
 
+    def display(self):
+        print(Student)
+        print(Student.school)
+        print(self.n)
+        print(Student.schoolcity)
+        print(Student.grade)
+
+Student.grade="10"
+obj=Student("aditya")
+obj1=Student("adi")
+print(obj.school)
+print(obj1.school,obj1.n)
+print(Student.school)
+obj.addnew()
+obj.display()
+
+# local variable:-scope dependent
+class students:
+    def __init__(self,name):
+        grade="20"
+        self.name=name
+        print(grade)
+
+    def grade(self):
+        print(grade)
+        
+obj=students("anuuu")
+obj.grade()
